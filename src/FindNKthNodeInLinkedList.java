4fd0a1692ce660105e90a9d4f59c7af47c508184
jiehuang <5761938+Nattime@users.noreply.github.com>
2022-03-25 18:36:53 -0400

    Added "Find n/k th node in Linked List"

---
+++ src/FindNKthNodeInLinkedList.java
@@ -0,0 +1,22 @@
+public class FindNKthNodeInLinkedList {
+    public static int nknode(Node head, int k)
+    {
+        // add your code here
+        if(head == null){
+            return -1;
+        }
+        if(head.next == null){
+            return head.data;
+        }
+        int count = 1;
+        Node trav = head.next;
+        while(trav != null){
+            if(count % k == 0){
+                head = head.next;
+            }
+            count++;
+            trav = trav.next;
+        }
+        return head.data;
+    }
+}
--- src/GFG.java
+++ src/GFG.java
@@ -1,7 +1,7 @@
 public class GFG {
     public static void main(String[] args) {
 
-        /* First and last occurrences of x test
+        /* First and last occurrences of x
         FirstAndLastOccurrencesOfX flox = new FirstAndLastOccurrencesOfX();
         long arr[] = new long[]{1, 3, 5, 5, 5, 5, 67, 123, 125};
         Print.print(flox.find(arr, 9, 5));
@@ -9,28 +9,25 @@ public class GFG {
         Print.print(flox.find(arr2, 9, 7));
         */
 
-        /* Sorted insert for circular linked list test
+        /* Sorted insert for circular linked list
         SortedInsertForCircularLinkedList sifcll = new SortedInsertForCircularLinkedList();
         int arr[] = new int[]{1, 2, 4};
-        Node head = createCircularLinkedList(arr);
+        Node head = Create.createCircularLinkedList(arr);
         Print.printCircular(SortedInsertForCircularLinkedList.sortedInsert(head, 2));
         int arr2[] = new int[]{1, 4, 7, 9};
-        Node head = createCircularLinkedList(arr);
+        Node head = Create.createCircularLinkedList(arr);
         Print.printCircular(SortedInsertForCircularLinkedList.sortedInsert(head, 5));
+         */
+
+        /* Find n/k th node in Linked List
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6};
+        Node head = Create.createNodeLinkedList(arr);
+        Print.print(FindNKthNodeInLinkedList.nknode(head, 2));
+        int[] arr2 = new int[]{2, 7, 9, 3, 5};
+        Node head2 = Create.createNodeLinkedList(arr2);
+        Print.print(FindNKthNodeInLinkedList.nknode(head2, 3));
          */
     }
 
-    static Node createCircularLinkedList(int[] arr){
-        if(arr == null || arr.length == 0){
-            return null;
-        }
-        Node head = new Node(arr[0]);
-        Node trav = head;
-        for(int i = 1; i < arr.length; i++){
-            trav.next = new Node(arr[i]);
-            trav = trav.next;
-        }
-        trav.next = head;
-        return head;
-    }
+
 }
