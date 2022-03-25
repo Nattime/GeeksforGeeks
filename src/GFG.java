96f2c972723e721612eed9ef4c15c89f116eae2b
jiehuang <5761938+Nattime@users.noreply.github.com>
2022-03-25 18:13:48 -0400

    Added "Sorted insert for circular linked list" problem

--- src/GFG.java
+++ src/GFG.java
@@ -9,5 +9,28 @@ public class GFG {
         Print.print(flox.find(arr2, 9, 7));
         */
 
+        /* Sorted insert for circular linked list test
+        SortedInsertForCircularLinkedList sifcll = new SortedInsertForCircularLinkedList();
+        int arr[] = new int[]{1, 2, 4};
+        Node head = createCircularLinkedList(arr);
+        Print.printCircular(SortedInsertForCircularLinkedList.sortedInsert(head, 2));
+        int arr2[] = new int[]{1, 4, 7, 9};
+        Node head = createCircularLinkedList(arr);
+        Print.printCircular(SortedInsertForCircularLinkedList.sortedInsert(head, 5));
+         */
+    }
+
+    static Node createCircularLinkedList(int[] arr){
+        if(arr == null || arr.length == 0){
+            return null;
+        }
+        Node head = new Node(arr[0]);
+        Node trav = head;
+        for(int i = 1; i < arr.length; i++){
+            trav.next = new Node(arr[i]);
+            trav = trav.next;
+        }
+        trav.next = head;
+        return head;
     }
 }
--- src/Print.java
+++ src/Print.java
@@ -2,12 +2,25 @@ import java.util.ArrayList;
 
 public class Print {
     public static void print(ArrayList<Long> list){
-        if(list.isEmpty() || list == null){
+        if(list == null || list.isEmpty()){
             System.out.println("Is empty");
         }else{
             for(Long iter : list){
                 System.out.print(iter + " ");
             }System.out.println();
+        }
+    }
+    public static void printCircular(Node n){
+        if(n == null){
+            System.out.println("Is Empty");
+        }else{
+            System.out.print(n.data);
+            Node trav = n.next;
+            while(trav != n){
+                System.out.print(" " + trav.data);
+                trav = trav.next;
+            }
+            System.out.println();
         }
     }
 }
