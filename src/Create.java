452ba7e5a119e39c09fd006523ddc3cc4175099d
jiehuang <5761938+Nattime@users.noreply.github.com>
2022-04-01 20:11:28 -0400

    Added createBST(int[])

--- src/Create.java
+++ src/Create.java
@@ -1,4 +1,33 @@
 public class Create {
+    static Node createBST(int[] arr){
+        if(arr.length < 1){
+            return null;
+        }
+        Node root = new Node(arr[0]);
+        for(int i = 1; i < arr.length; i++){
+            bstAdd(root, arr[i]);
+        }
+        return root;
+    }
+    static void bstAdd(Node root, int num){
+        if(root != null){
+            if(root.data > num){
+                if(root.prev != null){
+                    bstAdd(root.prev, num);
+                }else{
+                    root.prev = new Node(num);
+                }
+            }else if(root.data < num){
+                if(root.next != null){
+                    bstAdd(root.next, num);
+                }else{
+                    root.next = new Node(num);
+                }
+            }
+        }
+
+
+    }
     static Node createNodeLinkedList(int[] arr){
         if(arr == null || arr.length == 0){
             return null;
