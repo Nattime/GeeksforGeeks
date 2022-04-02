92130951fc0a8332377c94a31282c835f5453c76
jiehuang <5761938+Nattime@users.noreply.github.com>
2022-04-01 20:10:31 -0400

    Added methods printInOrder(Node), printPostOrder(Node), printPreOrder(Node)

--- src/Print.java
+++ src/Print.java
@@ -21,6 +21,34 @@ public class Print {
         System.out.println(num);
     }
 
+    public static void printInOrder(Node node){
+        printInOrderHelper(node);
+        System.out.println();
+    }
+    private static void printInOrderHelper(Node node){
+        if(node != null){
+            printInOrderHelper(node.prev);
+            System.out.print(node.data + " ");
+            printInOrderHelper(node.next);
+        }
+    }
+    public static void printPreOrderHelper(Node node){
+        if(node != null){
+            System.out.print(node.data + " ");
+            printPreOrderHelper(node.prev);
+            printPreOrderHelper(node.next);
+        }
+    }
+    public static void print(String str){
+        System.out.println(str);
+    }
+    public static void printPostOrderHelper(Node node){
+        if(node != null){
+            printPostOrderHelper(node.prev);
+            printPostOrderHelper(node.next);
+            System.out.print(node.data + " ");
+        }
+    }
     public static void print(List list){
         if(list == null || list.isEmpty()){
             System.out.println("Is empty");
@@ -42,5 +70,15 @@ public class Print {
             }
             System.out.println();
         }
+    }
+
+    public static void printPreOrder(Node root) {
+        printPreOrderHelper(root);
+        System.out.println();
+    }
+
+    public static void printPostOrder(Node root) {
+        printPostOrderHelper(root);
+        System.out.println();
     }
 }
