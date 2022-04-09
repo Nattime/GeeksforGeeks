75d0c43f1cb05c6f35155faa59a8310e1bfa5239
jiehuang <5761938+Nattime@users.noreply.github.com>
2022-04-09 17:15:43 -0400

    Added createBinaryTree(int[]) to create binary tree Added transformTree(Node) to turn all -1 nodes into null

--- src/Create.java
+++ src/Create.java
@@ -1,4 +1,58 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Create {
+    static Node createBinaryTree(int[] arr){
+        if(arr.length < 1){
+            return null;
+        }
+        Node root = new Node(arr[0]);
+
+        for(int i = 1; i < arr.length; i++){
+            binaryTreeAdd(root, arr[i]);
+        }
+        transformTree(root);
+        return root;
+    }
+    static void transformTree(Node root){
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            Node curr = q.remove();
+
+            if(curr.left != null){
+                if(curr.left.data == -1){
+                    curr.left = null;
+                }else{
+                    q.add(curr.left);
+                }
+            }
+            if(curr.right != null){
+                if(curr.right.data == -1){
+                    curr.right = null;
+                }else{
+                    q.add(curr.right);
+                }
+            }
+        }
+    }
+    static void binaryTreeAdd(Node root, int num){
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            Node curr = q.remove();
+            if(curr.left == null){
+                curr.left = new Node(num);
+                return;
+            }
+            q.add(curr.left);
+            if(curr.right == null){
+                curr.right = new Node(num);
+                return;
+            }
+            q.add(curr.right);
+        }
+    }
     static Node createBST(int[] arr){
         if(arr.length < 1){
             return null;
