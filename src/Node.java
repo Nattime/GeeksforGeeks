8fb8bc383d9bcdd844a47162aec1e2248567cd74
jiehuang <5761938+Nattime@users.noreply.github.com>
2022-04-16 17:10:38 -0400

    Added setters and getters for data, next, prev, left, right

--- src/Node.java
+++ src/Node.java
@@ -2,7 +2,43 @@ public class Node {
     int data;
     Node next, prev, left, right;
     Node(int data){
-        this.data = data;
+        setData(data);
         next = prev = left = right = null;
+    }
+    void setData(int data){
+        this.data = data;
+    }
+    void setNext(Node n){
+        right = next = n;
+    }
+    void setPrev(Node n){
+        left = prev = n;
+    }
+    void setLeft(Node n){
+        setPrev(n);
+    }
+    void setRight(Node n){
+        setNext(n);
+    }
+    int getData(){
+        return data;
+    }
+    Node getPrev(){
+        if(left != null){
+            return left;
+        }
+        return prev;
+    }
+    Node getNext(){
+        if(right != null){
+            return right;
+        }
+        return next;
+    }
+    Node getLeft(){
+        return getPrev();
+    }
+    Node getRight(){
+        return getNext();
     }
 }
