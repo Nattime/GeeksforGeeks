1c5a2b60a5e3d07caf604acd370db6fbe03bffda
jiehuang <5761938+Nattime@users.noreply.github.com>
2022-04-16 19:57:52 -0400

    Added variable: random, left, right Added constructor Tree(int, Tree, Tree), Tree(int, Tree, Tree, Tree), Tree(int, int, int) Added setRandom(Tree)

--- src/Tree.java
+++ src/Tree.java
@@ -1,6 +1,26 @@
 public class Tree extends Node{
+    Tree random, left, right;
 
     Tree(int data) {
         super(data);
+    }
+    Tree(int data, Tree l, Tree r){
+        this.data = data;
+        left = l;
+        right = r;
+    }
+    Tree(int data, Tree l, Tree r, Tree random){
+        this.data = data;
+        left = l;
+        right = r;
+        this.random = random;
+    }
+    Tree(int data, int l, int r){
+        this.data = data;
+        left = new Tree(l);
+        right = new Tree(r);
+    }
+    void setRandom(Tree tree){
+        random = tree;
     }
 }
