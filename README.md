8833754c3756572a996ec7d6deffcd2b4f84dce2
jiehuang <5761938+Nattime@users.noreply.github.com>
2022-04-23 21:05:13 -0400

    Added AdditionOfSubmatrix

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Geeks for Geeks Completed Coding Challenges
 ## Finished challenges
+Addition of submatrix
+
 Akku and Binary Numbers
 
 Break Numbers[Duplicate problem]
--- src/GFG.java
+++ src/GFG.java
@@ -3,8 +3,19 @@ import java.util.Stack;
 public class GFG{
     public static void main(String[] args){
 
+        /* Addition of submatrix
+        AdditionOfSubmatrix aos = new AdditionOfSubmatrix();
+        int[][] matrix = {{1, 2, 3, 4, 5, 6}, {7, 8, 9, 10, 11, 12}, {13, 14, 15, 16, 17, 18}, {19, 20, 21, 22, 23, 24}, {25, 26, 27, 28, 29, 30}};
+        Print.print(aos.subMatrixSum(matrix, 5, 6, 3, 4, 4, 5));
+        int[][] matrix2 = {{9, 8, 7},{4, 2, 1},{6, 5, 3}};
+        Print.print(aos.subMatrixSum(matrix2, 3, 3, 1, 2, 3, 3));
+
+         */
+
+
 //        Print.testPrint();
 //        Print.print(1);
+
 
         /* Akku and Binary Numbers
         AkkuAndBinaryNumbers aabn = new AkkuAndBinaryNumbers();
