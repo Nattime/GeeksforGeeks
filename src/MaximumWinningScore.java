8c9f5e6d3f1af06b52f10ec3b79ea1739f1355e9
jiehuang <5761938+Nattime@users.noreply.github.com>
2022-05-10 16:56:05 -0400

    Added MaximumWinningScore

--- src/MaximumWinningScore.java
+++ src/MaximumWinningScore.java
@@ -1,13 +1,9 @@
 public class MaximumWinningScore{
     public static Long findMaxScore(Node root){
         //code here
-        return help(root, 1L);
-    }
-
-    public static Long help(Node node, Long mul){
-        if(node == null){
-            return mul;
+        if(root == null){
+            return 1L;
         }
-        return Math.max(help(node.left, mul * node.data), help(node.right, mul * node.data));
+        return root.data * Math.max(findMaxScore(root.left), findMaxScore(root.right));
     }
 }
