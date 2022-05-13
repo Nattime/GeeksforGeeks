72d899c9d68012a3b90e2fe523f604ced78272fb
jiehuang <5761938+Nattime@users.noreply.github.com>
2022-05-13 17:35:04 -0400

    Added AryasLongString

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Geeks for Geeks Completed Coding Challenges
 ## Finished challenges
+Arya's Long String
+
 Ordering of strings
 
 Starting Point
---
+++ src/AryasLongString.java
@@ -0,0 +1,18 @@
+public class AryasLongString{
+    public static int fun(String s, int k, int n, char c){
+        // Your code here
+        int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == c){
+                count++;
+            }
+        }
+        count *= (int)(n / s.length());
+        for(int i = 0; i < ((n % s.length())); i++){
+            if(s.charAt(i) == c){
+                count++;
+            }
+        }
+        return count;
+    }
+}
