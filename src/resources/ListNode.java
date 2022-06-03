eb17e07f41a6a4d4ed87de03f5fd3a66b6d677fb
jiehuang <5761938+Nattime@users.noreply.github.com>
2022-06-03 19:04:01 -0400

    Added Resources: TreeNode, ListNode

---
+++ src/resources/ListNode.java
@@ -0,0 +1,27 @@
+package resources;
+
+public class ListNode{
+    public int val;
+    public ListNode next;
+
+    public ListNode(){
+    }
+
+    @Override
+    public String toString(){
+        return val + "->" + next;
+    }
+
+    public ListNode(int val){
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+
+    public ListNode getNext(){
+        return next;
+    }
+}
